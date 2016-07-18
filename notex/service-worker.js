072a16fbdd6ea68922138ea6db0332bcc4eb80e9
Narendra Singh <narendrasingh@outlook.in>
2016-07-18 12:48:01 +0530

    Notex(PWA)
    its progressive webapp now  ;)

---
+++ notex/service-worker.js
@@ -0,0 +1,47 @@
+var cacheName = 'notex7';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/images/128x128.png',
+  '/images/144x144.png',
+  '/images/152x152.png',
+  '/images/192x192.png',
+  '/images/256x256.png',
+  '/images/favicon.ico',
+  '/manifest.json',  
+  '/DroidSerif.ttf'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
